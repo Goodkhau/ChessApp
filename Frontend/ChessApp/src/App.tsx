22163e0634b2bde98f2b3cc0fa69d39ad59d56ed
Goodkhau <gos.khai12@gmail.com>
2026-03-09 20:46:28 -0500

    Added homepage to routes, need to fix styles and keep it mobile friendly.

--- Frontend/ChessApp/src/App.tsx
+++ Frontend/ChessApp/src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Homepage from "./Homepage"
 import ChessElement from "./ChessElement"
 
 
 export default function App() {
     return (
         <BrowserRouter>
-             <Routes>
-                 <Route path="/" element={<ChessElement />} />
-             </Routes>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/Chess/:modelName" element={<ChessElement />} />
+            </Routes>
         </BrowserRouter>
     );
 }
