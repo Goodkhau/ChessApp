c21aee76013066b3911d6c69758211d645813512
Goodkhau <gos.khai12@gmail.com>
2026-04-23 12:24:53 -0500

    Added animation to dropdown

--- Frontend/ChessApp/src/pages/ChessAI/ModelName/components/ExpandableListComponent.tsx
+++ Frontend/ChessApp/src/pages/ChessAI/ModelName/components/ExpandableListComponent.tsx
@@ -11,9 +11,9 @@ export default function ExpandableListComponent({ instanceKey }: { instanceKey: 
 		<article className="h-10 w-full bg-blue-800/10 rounded-xl">
 			<hgroup className="h-8 py-5" onClick={() => setIsOpen(!isOpen)}>Header</hgroup>
 			<article className={`relative z-10 bg-gray-800/85 border-gray-700 rounded-xl
-				${predictionList.length ? "border-3" : ""}
-				${isOpen ? 'w-auto' : 'hidden'}`}
-			>
+				transition-all duration-300 ease-linear overflow-y-scroll
+				${isOpen ? `max-h-100 ${predictionList.length ? 'border-3' : ''}` : 'max-h-0 border-0'}
+			`}>
 				{_.map(predictionList, prediction => (
 					<article className="flex flex-row items-center m-2 px-2 bg-teal-800/20 hover:bg-teal-600/20 rounded-xl">
 						<h2 className="w-2/15 p-0.5 text-1xl text-gray-400">{prediction.move}</h2>
