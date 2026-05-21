d03a0298d685543b111adfe20a7e3c7d9211b948
Goodkhau <gos.khai12@gmail.com>
2026-05-21 17:51:24 -0500

    Better augmented the animations

--- Frontend/src/pages/Animations/PageAnimation.tsx
+++ Frontend/src/pages/Animations/PageAnimation.tsx
@@ -25,7 +25,7 @@ export default function PageTransition({ children }: PageTransitionProps) {
 			initial="initial"
 			animate="animate"
 			exit="exit"
-			transition={{ duration: 0.3 }}
+			transition={{ duration: 0.1 }}
 		>
 			{children}
 		</motion.main>
