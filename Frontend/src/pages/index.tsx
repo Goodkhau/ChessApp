da423390bb05796ffb5017731c5440b1c1dc3dba
Goodkhau <gos.khai12@gmail.com>
2026-05-23 15:57:38 -0500

    Updated name

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "react";
 import AboutPage from "./AboutPage/AboutPage.tsx";
 import ChessPage from "./ChessAI/ChessPage.tsx";
-import ChessAIHomepage from "./Homepage/index.tsx";
+import ChessAIHomepage from "./Homepage/Homepage.tsx";
 
 export type pageTitles = "Home" | "ChessAI" | "About";
 
