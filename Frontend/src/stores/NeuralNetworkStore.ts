0dc5c7fdb8537ae04f8b34b564ac9db51a7b497d
Goodkhau <gos.khai12@gmail.com>
2026-05-31 17:43:15 -0500

    updating poition of neurons

--- Frontend/src/stores/NeuralNetworkStore.ts
+++ Frontend/src/stores/NeuralNetworkStore.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { create } from "zustand";
 
 interface LayerConfig {
@@ -15,6 +16,8 @@ interface Layer {
 
 interface Neuron {
     weight: number;
+	x: number;
+	y: number;
 }
 
 interface Connection {
@@ -46,7 +49,7 @@ const useNeuralNetworkStore = create<State & Actions>()((set) => ({
 	layers: {},
 	connection: {},
 
-	createNetwork() {
+	createNetwork: () => {
 		const NeuralNetwork: State = { layers: {}, connection: {} };
 		for (let currentLayer = 0; currentLayer < LAYERS_CONF.length; currentLayer++) {
 			const id = !currentLayer
@@ -61,19 +64,29 @@ const useNeuralNetworkStore = create<State & Actions>()((set) => ({
 				neurons: {},
 			};
 
+			const maxCount = _.maxBy(LAYERS_CONF, "neuronCount")?.neuronCount;
+			if (maxCount === undefined)
+				throw new Error();
+
 			for (let currentNeuron = 0; currentNeuron < LAYERS_CONF[currentLayer].neuronCount; currentNeuron++) {
 				const _n = `n-${currentNeuron}`;
+				const displacement = (1440 / maxCount * (maxCount - LAYERS_CONF[currentLayer].neuronCount)) / 2;
+				const placement = (1440 / maxCount) * currentNeuron;
 				NeuralNetwork.layers[id].neurons[_n] = {
 					weight: Math.random(),
+					x: 1440 / currentLayer,
+					y: placement + displacement,
 				};
 			}
 		}
+
+		//Create connections
 
 		set(() => NeuralNetwork);
 	},
 }));
 
 export {
-    useNeuralNetworkStore
+	useNeuralNetworkStore
 };
 
