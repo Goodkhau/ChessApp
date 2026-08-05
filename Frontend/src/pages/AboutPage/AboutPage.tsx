a7cde68821ed0cc322b665c081693a5b0b8bbf33
Goodkhau <gos.khai12@gmail.com>
2026-08-04 21:11:18 -0500

    Updating UI in about page

--- Frontend/src/pages/AboutPage/AboutPage.tsx
+++ Frontend/src/pages/AboutPage/AboutPage.tsx
@@ -1,17 +1,24 @@
+import _ from "lodash";
 import ReactMarkdown from "react-markdown";
-import content from "./components/markdown-guide.md?raw";
+import Little_Blue from "./components/Little_Blue.md?raw";
+import guide from "./components/markdown-guide.md?raw";
+
+const Markdowns = [
+	{ title: "Guide", file: guide },
+	{ title: "Little Blue", file: Little_Blue },
+];
 
 export default function AboutPage() {
 	return (
-		<section className="h-screen w-screen flex justify-center gap-10 my-24">
-			<details className="max-w-[100vw] p-2 prose relative rounded-xl">
-				<summary className="p-4 w-[80vw] bg-slate-800/90 rounded-t-xl">Little Blue</summary>
-				<div className="p-6 absolute w-[80vw] text-white/60 bg-slate-800">
-					<ReactMarkdown>
-						{content}
-					</ReactMarkdown>
-				</div>
-			</details>
+		<section className="h-screen w-full flex flex-col items-center gap-2 py-24">
+			{_.map(Markdowns, ({ title, file }) => (
+				<details className="max-w-[100vw] p-2 prose rounded-xl">
+					<summary className="p-4 w-[80vw] bg-slate-800/90 rounded-t-xl">{title}</summary>
+					<div className="p-6 w-[80vw] overflow-clip text-white/60 bg-slate-800">
+						<ReactMarkdown>{file}</ReactMarkdown>
+					</div>
+				</details>
+			))}
 		</section>
 	);
 }
